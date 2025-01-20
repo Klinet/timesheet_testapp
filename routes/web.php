009da3d8acc2780715c3d3d5375336c6679fd08b
Klinet <akali.klinet@gmail.com>
2025-01-20 12:06:28 +0100

    [dev] routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProjectController::class, 'index']);
+
+Route::resource('projects', ProjectController::class);
+
+Route::post('projects/{project}/startTiming', [ProjectController::class, 'startTiming'])->name('projects.startTiming');
+Route::post('projects/{project}/stopTiming', [ProjectController::class, 'stopTiming'])->name('projects.stopTiming');
+
+
+
